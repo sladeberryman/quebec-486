f35fccf73091fef1ec6e8b03419758c971a9ba9f
sladeberryman <slade12berryman@gmail.com>
2024-03-04 19:31:03 -0600

    finish app

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 5500;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const bodyParser = require('body-parser')
+const { MongoClient, ObjectId, ServerApiVersion } = require('mongodb');
+const bodyParser = require('body-parser');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // set the view engine to ejs
 let path = require('path');
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
-app.use(bodyParser.urlencoded({ extended: true }))
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGO_URI, {
@@ -18,131 +23,130 @@ const client = new MongoClient(process.env.MONGO_URI, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+// CSRF middleware
+const csrfProtection = csrf({ cookie: true });
+app.use(csrfProtection);
 
 async function getNameData() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    const result = await client.db("quebec-database").collection("quebec-collection").find().toArray();
-    console.log("mongo call await inside f/n: ", result);
-    return result; 
-  } 
-  catch(err) {
-    console.log("getName() error: ", err);
-  }
-  finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
+    const result = await client
+      .db('quebec-database')
+      .collection('quebec-collection')
+      .find()
+      .toArray();
+    console.log('mongo call await inside f/n: ', result);
+    return result;
+  } catch (err) {
+    console.log('getName() error: ', err);
   }
 }
 
-//reading from mongo
-app.get('/', async (req, res) =>  {
+app.get('/', async (req, res) => {
+  try {
+    let result = await getNameData().catch(console.error);
+    console.log('getNameData() Result: ', result);
 
-  let result = await getNameData().catch(console.error); ; 
-
-  console.log("getNameData() Result: ", result);
-
-  res.render('index', {
-   
-    pageTitle: "Name Game", 
-    jobData: result 
-
-  });
-  
+    res.render('index', {
+      pageTitle: 'Name App',
+      nameData: result,
+      csrfToken: req.csrfToken(), // Include CSRF token in the render
+    });
+  } catch (error) {
+    console.error('Error rendering view:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-//create to mongo 
-app.post('/addName', async (req, res) => {
 
+// add a name
+app.post('/addName', async (req, res) => {
   try {
-    client.connect; 
-    const collection = client.db("quebec-database").collection("quebec-collection");
-    
-    //draws from body parser 
+    const collection = client
+      .db('quebec-database')
+      .collection('quebec-collection');
     console.log(req.body);
-    
     await collection.insertOne(req.body);
-      
+    res.redirect('/');
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+// update a name
+app.get('/updateName', async (req, res) => {
+  try {
+    const devId = req.query.devId;
+    console.log('Received request to update name with ID:', devId);
+
+    const collection = client
+      .db('quebec-database')
+      .collection('quebec-collection');
+    const objectId = new ObjectId(devId);
+
+    const nameToUpdate = await collection.findOne({ _id: objectId });
+
+    res.render('update', {
+      pageTitle: 'Update Name Info',
+      nameData: nameToUpdate,
+      csrfToken: req.csrfToken(), // Include CSRF token in the render
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// handle update name form submission
+app.post('/updateName', async (req, res) => {
+  try {
+    const devId = req.body.devId;
+    console.log('Received request to update name with ID:', devId);
+
+    const collection = client
+      .db('quebec-database')
+      .collection('quebec-collection');
+    const objectId = new ObjectId(devId);
+
+    // Update the name with new data from the form
+    await collection.updateOne(
+      { _id: objectId },
+      { $set: { name: req.body.name, pigLatinName: req.body.pigLatinName, spiritAnimal: req.body.spiritAnimal } }
+    );
 
     res.redirect('/');
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
   }
-  catch(err){
-    console.log(err)
-  }
-  finally{
-   // client.close()
-  }
+});
 
-})
 
-// update in Mongo
-app.post('/updateName/:id', async (req, res) => {
-
+// delete that name
+app.post('/deleteName', async (req, res) => {
   try {
-    console.log("req.parms.id: ", req.params.id) 
-    
-    client.connect; 
-    const collection = client.db("quebec-database").collection("quebec-collection");
-    let result = await collection.findOneAndUpdate( 
-      {"_id": ObjectId(req.params.id)}, { $set: {"age": "Old" } }
-    )
-    .then(result => {
-      console.log(result); 
-      res.redirect('/');
-    })
-    .catch(error => console.error(error))
+    const devId = req.body.devId;
+    console.log('Received request to delete user with ID:', devId);
+
+    const collection = client
+      .db('quebec-database')
+      .collection('quebec-collection');
+    const objectId = new ObjectId(devId);
+
+    const result = await collection.findOneAndDelete({ _id: objectId });
+
+    console.log('Result:', result);
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
   }
-  finally{
-    //client.close()
-  }
-
-})
-
-// delete in Mongo
-app.post('/deleteName/:id', async (req, res) => {
-
-  try {
-    console.log("req.parms.id: ", req.params.id) 
-    
-    client.connect; 
-    const collection = client.db("quebec-database").collection("quebec-collection");
-    let result = await collection.findOneAndDelete( 
-      {
-        "_id": ObjectId(req.params.id)
-      }
-    )
-    .then(result => {
-      console.log(result); 
-      res.redirect('/');
-    })
-    .catch(error => console.error(error))
-  }
-  finally{
-    //client.close()
-  }
-
-})
-
-app.get('/name', (req,res) => {
-
-  console.log("in get to slash name:", req.query.ejsFormName); 
-  myTypeServer = req.query.ejsFormName; 
-
-  res.render('index', {
-    myTypeClient: myTypeServer,
-    myResultClient: "myResultServer"
-
-  });
-
-  
-})
-
+});
 
 app.listen(port, () => {
-console.log(`QUEBEC app listening on port ${port}`)
-})
+  console.log(`QUEBEC Name app listening on port ${port}`);
+});
